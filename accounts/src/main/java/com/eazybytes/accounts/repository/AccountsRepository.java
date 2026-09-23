c8d0b7027b08323f85bd65f648290cf9e621560d
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-09-23 18:18:57 +0530

    [Microservices]:[Account]: Created create account api for the bank customer..

--- accounts/src/main/java/com/eazybytes/accounts/repository/AccountsRepository.java
+++ accounts/src/main/java/com/eazybytes/accounts/repository/AccountsRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AcountsRepository extends JpaRepository<Accounts, Long> {
+public interface AccountsRepository extends JpaRepository<Accounts, Long> {
 
     
 }
