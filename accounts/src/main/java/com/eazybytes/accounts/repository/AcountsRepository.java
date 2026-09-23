826ec9014b321a67691b81a8251b0a22f0bbb7ff
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-09-23 17:21:31 +0530

    [Microservices]:[Account]:Created repository classes for accounts and customer information.

---
+++ accounts/src/main/java/com/eazybytes/accounts/repository/AcountsRepository.java
@@ -0,0 +1,13 @@
+package com.eazybytes.accounts.repository;
+
+
+import com.eazybytes.accounts.entity.Accounts;
+import com.eazybytes.accounts.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AcountsRepository extends JpaRepository<Accounts, Long> {
+
+    
+}
