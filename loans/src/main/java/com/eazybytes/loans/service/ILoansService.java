ab7a4a2b835faa697cf063bf0c4a44b37db1e97a
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-09-24 13:54:36 +0530

    [Microservices]:[Loans]: Created service layer for loan apis.

---
+++ loans/src/main/java/com/eazybytes/loans/service/ILoansService.java
@@ -0,0 +1,13 @@
+package com.eazybytes.loans.service;
+
+
+import com.eazybytes.loans.dto.LoansDto;
+
+public interface ILoansService {
+
+    /**
+     *
+     * @param mobileNumber - Mobile Number of the Customer
+     */
+    void createLoan(String mobileNumber);
+}
