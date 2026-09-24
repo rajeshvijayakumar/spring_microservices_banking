57dcb8a59f2493a32e75a9a85b227648cfdc9dd3
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-09-24 13:02:38 +0530

    [Microservices]:[Account]:Integrated Open API document specification with swagger-ui..

--- accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -1,5 +1,6 @@
 package com.eazybytes.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
@@ -8,13 +9,23 @@ import lombok.Data;
 
 
 @Data
+@Schema(
+        name = "Accounts",
+        description = "Schema to hold Account information"
+)
 public class AccountsDto {
 
     @NotEmpty(message = "AccountNumber can not be a null or empty")
     @Pattern(regexp="(^$|[0-9]{10})",message = "AccountNumber must be 10 digits")
+    @Schema(
+            description = "Account Number of Eazy Bank account", example = "3454433243"
+    )
     private Long accountNumber;
 
     @NotEmpty(message = "AccountType can not be a null or empty")
+    @Schema(
+            description = "Account type of Eazy Bank account", example = "Savings"
+    )
     private String accountType;
 
     @NotEmpty(message = "BranchAddress can not be a null or empty")
