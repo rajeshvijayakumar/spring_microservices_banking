37aa986182fe777f836ea1622bd83c2c1fd3c087
rajeshvijayakumar <rajeshvj.learn@gmail.com>
2026-09-24 14:38:44 +0530

    [Microservices]:[Loans]:Enabled auditing for auditing columns createdat, createdby, updatedat, updatedby.

--- loans/src/main/java/com/eazybytes/loans/entity/BaseEntity.java
+++ loans/src/main/java/com/eazybytes/loans/entity/BaseEntity.java
@@ -1,40 +1,44 @@
 package com.eazybytes.loans.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
+
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 
-@MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @ToString
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class) // need to set the listener for auditing
 public class BaseEntity {
 
-    @CreatedDate
-    @Column(updatable = false)
-    private LocalDateTime createdAt;
+    @Column(name = "created_at", updatable = false)
+    @CreatedDate // need to set during auditing
+    @CreationTimestamp
+    private Instant createdAt;
 
-    @CreatedBy
-    @Column(updatable = false)
+    @CreatedBy  // need to set during auditing
+    @Column(name = "created_by", length = 20, updatable = false)
     private String createdBy;
 
-    @LastModifiedDate
-    @Column(insertable = false)
-    private LocalDateTime updatedAt;
+    @LastModifiedDate  // need to set during auditing
+    @UpdateTimestamp
+    @Column(name = "updated_at", insertable = false)
+    private Instant updatedAt;
 
-    @LastModifiedBy
-    @Column(insertable = false)
+    @Column(name = "updated_by", length = 20, insertable = false)
+    @LastModifiedBy  // need to set during auditing
     private String updatedBy;
 
 }
